980f68e544a26a090ac9aba7fd1503afcfd7e530
AleksanderMikhaylin <142292071+AleksanderMikhaylin@users.noreply.github.com>
2025-02-04 09:27:50 +1100

    Commit main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import ShopItemClass from './components/ShopItemClass.jsx';
 import item from './items/item.js';
